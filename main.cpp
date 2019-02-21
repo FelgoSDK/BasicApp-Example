7fe322aea6e419a562d2bcfe0cda51be85c9f843
Günther Teufl <guenther.teufl@gmail.com>
2019-02-21 16:24:41 +0100

    Update project for Felgo 3.0

--- main.cpp
+++ main.cpp
@@ -1,38 +1,38 @@
 #include <QApplication>
-#include <VPApplication>
+#include <FelgoApplication>
 
 #include <QQmlApplicationEngine>
 
 // uncomment this line to add the Live Client Module and use live reloading with your custom C++ code
-//#include <VPLiveClient>
+//#include <FelgoLiveClient>
 
 
 int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
-  VPApplication vplay;
+  FelgoApplication felgo;
 
-  // Use platform-specific fonts instead of V-Play's default font
-  vplay.setPreservePlatformFonts(true);
+  // Use platform-specific fonts instead of Felgo's default font
+  felgo.setPreservePlatformFonts(true);
 
   QQmlApplicationEngine engine;
-  vplay.initialize(&engine);
+  felgo.initialize(&engine);
 
   // use this during development
   // for PUBLISHING, use the entry point below
-  vplay.setMainQmlFileName(QStringLiteral("qml/Main.qml"));
+  felgo.setMainQmlFileName(QStringLiteral("qml/Main.qml"));
 
   // use this instead of the above call to avoid deployment of the qml files and compile them into the binary with qt's resource system qrc
   // this is the preferred deployment option for publishing games to the app stores, because then your qml files and js files are protected
   // to avoid deployment of your qml files and images, also comment the DEPLOYMENTFOLDERS command in the .pro file
   // also see the .pro file for more details
-  // vplay.setMainQmlFileName(QStringLiteral("qrc:/qml/Main.qml"));
+  // felgo.setMainQmlFileName(QStringLiteral("qrc:/qml/Main.qml"));
 
-  engine.load(QUrl(vplay.mainQmlFileName()));
+  engine.load(QUrl(felgo.mainQmlFileName()));
 
-  // to start your project as Live Client, comment (remove) the lines "vplay.setMainQmlFileName ..." & "engine.load ...",
+  // to start your project as Live Client, comment (remove) the lines "felgo.setMainQmlFileName ..." & "engine.load ...",
   // and uncomment the line below
-  //VPlayLiveClient client (&engine);
+  //FelgoLiveClient client (&engine);
 
   return app.exec();
 }
--- qml/Main.qml
+++ qml/Main.qml
@@ -1,16 +1,16 @@
-import VPlayApps 1.0
+import Felgo 3.0
 import QtQuick 2.0
 import "model"
 import "logic"
 import "pages"
 
 App {
-  // You get free licenseKeys from https://v-play.net/licenseKey
+  // You get free licenseKeys from https://felgo.com/licenseKey
   // With a licenseKey you can:
   //  * Publish your games & apps for the app stores
-  //  * Remove the V-Play Splash Screen or set a custom one (available with the Pro Licenses)
+  //  * Remove the Felgo Splash Screen or set a custom one (available with the Pro Licenses)
   //  * Add plugins to monetize, analyze & improve your apps (available with the Pro Licenses)
-  //licenseKey: "<generate one from https://v-play.net/licenseKey>"
+  //licenseKey: "<generate one from https://felgo.com/licenseKey>"
 
   // app initialization
   Component.onCompleted: {
